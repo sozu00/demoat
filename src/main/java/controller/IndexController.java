d6940af78881bfe25859c4a5f74d85690ffb7685
jiniguez <inhiguez.jesus@gmail.com>
2018-01-16 20:08:12 +0100

    update de pom.xml

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,12 +1,21 @@
 package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import model.User;
 import service.UserService;
 
-//@RestController
+@RestController
 public class IndexController {
 
 	@Autowired
 	UserService userService;
+	
+	
+	@RequestMapping("/")
+	public User findUser() {
+		return userService.findById(1);
+	}
 }
