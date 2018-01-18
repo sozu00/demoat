2974efd0be0cf07af2ef8a0cba1327e63069da95
sozu00 <inhiguez.jesus@gmail.com>
2018-01-18 23:49:45 +0100

    aplication.yml arreglado para mostrar db

--- src/main/java/service/implementation/CitaServiceImpl.java
+++ src/main/java/service/implementation/CitaServiceImpl.java
@@ -1,7 +1,6 @@
 package service.implementation;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import dao.CitaDAO;
 import model.Cita;
