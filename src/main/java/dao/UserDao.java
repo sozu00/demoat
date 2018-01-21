9d18dc5ce58c7a6ddd56f5fb15695960d00a8a5a
sozu00 <inhiguez.jesus@gmail.com>
2018-01-21 21:40:02 +0100

    limpiando repo

--- src/main/java/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import model.User;
-
-public interface UserDao extends PagingAndSortingRepository<User, Integer>{
-
-}
